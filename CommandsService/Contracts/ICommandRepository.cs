c8ef99d6e983927969cd95d3e055495e78c1c5aa
augusto <augustosaaude@gmail.com>
2024-03-16 16:13:17 -0300

    feat: create appDbContext, repository and contract of commandService

---
+++ CommandsService/Contracts/ICommandRepository.cs
@@ -0,0 +1,14 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.Contracts;
+
+public interface ICommandRepository
+{
+    bool SaveChanges();
+    IEnumerable<Platform> GetAllPlatforms();
+    void CreatePlatform(Platform platform);
+    bool PlatformExist(int platformId);
+    IEnumerable<Command> GetCommandsForPlatform(int platformId);
+    Command GetCommand(int platformId, int commandId);
+    void CreateCommand(int platformId, Command command);
+}
